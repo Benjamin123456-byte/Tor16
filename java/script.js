1121c41dd1bf700dd4cf253e3d5093ad86893bfe
Benjamin123456-byte <benjaminazmi.uslu@vbu.se>
2025-05-05 13:09:10 +0200

    Add files via upload

--- java/script.js
+++ java/script.js
@@ -87,3 +87,28 @@ function displayVisitorCount() {
 }
 
 document.addEventListener('DOMContentLoaded', displayVisitorCount);
+
+
+
+function setupScrollToTopButton() {
+    const scrollToTopButton = document.getElementById('scroll-to-top');
+
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            scrollToTopButton.style.display = 'block';
+        } else {
+            scrollToTopButton.style.display = 'none';
+        }
+    });
+
+
+    scrollToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', setupScrollToTopButton);
